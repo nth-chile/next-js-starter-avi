80829ff5af6fa561ea629b653b1dc3397d4df16e
Jared Salzano <jaredsalzano@gmail.com>
2020-11-26 10:09:01 -0500

    Setup

--- lib/db.ts
+++ lib/db.ts
@@ -4,8 +4,8 @@ export const db = mysql({
   config: {
     host: process.env.MYSQL_HOST,
     database: process.env.MYSQL_DATABASE,
-    user: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD
   },
 })
 
